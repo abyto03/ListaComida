17dc046410c7f58aa6610bcb09a35a5c5a7b8150
Armando <jarodriguezvera@gmail.com>
2024-02-18 16:23:58 -0600

    Change Theme, Colors and configure a different fontColor

--- app/src/main/java/com/mexiti/listacomida/MainActivity.kt
+++ app/src/main/java/com/mexiti/listacomida/MainActivity.kt
@@ -5,27 +5,36 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mexiti.listacomida.data.DataSource
 import com.mexiti.listacomida.model.Platillo
 import com.mexiti.listacomida.ui.theme.ListaComidaTheme
+import com.mexiti.listacomida.ui.theme.md_theme_dark_onSecondary
+import com.mexiti.listacomida.ui.theme.md_theme_dark_onTertiary
+import com.mexiti.listacomida.ui.theme.md_theme_light_onTertiary
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,21 +77,41 @@ fun MenuCardList( platilloList:List<Platillo>, modifier: Modifier = Modifier ){
 
 @Composable
 fun MenuCard(platillo: Platillo, modifier: Modifier = Modifier){
-    Card(modifier = Modifier) {
-        Column  {
+    Card(modifier = modifier
+        .padding(10.dp)) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        )  {
             Image(
                 painter = painterResource(id = platillo.drawableResourceId),
                 contentDescription = stringResource(id = platillo.stringResourceId),
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(190.dp),
+                    .size(180.dp)
+                    .padding(start = 10.dp)
+                    .clip(CircleShape)
+                ,
                 contentScale = ContentScale.Crop
             )
-            Text(
-                text = LocalContext.current.getString(platillo.stringResourceId),
-                modifier = Modifier.padding(22.dp),
-                style = MaterialTheme.typography.headlineMedium
-            )
+            Column(
+                modifier =
+                    Modifier.padding(start = 20.dp)
+            ) {
+                Text(
+                    text = "Pozole",
+                    style = MaterialTheme.typography.headlineSmall
+
+                )
+                Text(text = "MX $100.0 ")
+                Text(
+                    text = "Ahorra hasta el 30%",
+                    fontWeight = FontWeight.Bold,
+                    color = md_theme_dark_onSecondary
+                    
+                )
+            }
+
 
         }
 
@@ -94,7 +123,7 @@ fun MenuCard(platillo: Platillo, modifier: Modifier = Modifier){
 @Preview(showBackground = true)
 @Composable
 fun MenuPlatilloPreview() {
-    ListaComidaTheme {
+    ListaComidaTheme(darkTheme = false) {
         MenuCard(platillo = Platillo(R.string.pozole,R.drawable.pozole) )
     }
 }
